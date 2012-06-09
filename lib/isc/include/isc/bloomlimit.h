b1fb80f15506f8c470d384b0f5c87ad8acb71ea0
Tony Finch <dot@dotat.at>
2012-06-09 15:46:36 +0100

    Move some text to the in-source documentation.
    Add some notes on how exponential aging of the data in the
    hash table allows us to get a meaningful measure of a
    client's query rate. The limits will be handled by the
    calling code rather than being part of the Bloom filter
    itself.

--- lib/isc/include/isc/bloomlimit.h
+++ lib/isc/include/isc/bloomlimit.h
@@ -21,6 +21,35 @@
  * \brief
  * Rate limit clients using a Bloom filter
  *
+ * We store clients' query rate measurements in a hash table which is
+ * accessed like a Bloom filter, except that rather than being a
+ * present/absent bit, each entry is a rate measurement. A number of
+ * hashes are calculated for the current client, and the client's rate
+ * is the minimum of the buckets indexed by the hashes. To record a
+ * query, increment the rate value in all the minimum-valued hash
+ * buckets.
+ *
+ * There is a periodic task to age old data out of the hash table.
+ * Every p seconds all the buckets are multiplied by an aging factor
+ * 0 <= a < 1. If a client is querying at a constant rate r the
+ * measured value from the hash table r' comes from the sum of the
+ * resulting geometric progression, which is p * r / (1 - a). So to
+ * calculate the client's actual rate from the value in the hash table,
+ * r = r' * (1 - a) / p.
+ *
+ * The reference for this code is "network applications of Bloom filters"
+ * by Andrei Broder and Michael Mitzenmacher.
+ * http://www.eecs.harvard.edu/~michaelm/postscripts/im2005b.pdf
+ * The specific technique for measuring each client's rate is
+ * described in section 8.1.
+ *
+ * Also worth noting is "less hashing, same performance"
+ * by Adam Kirsch and Michael Mitzenmacher
+ * http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf
+ * which shows that Bloom filters work just as well with a linear
+ * combination of two independent hashes as they do with multiple
+ * independent hashes.
+ *
  */
 
 #include <isc/lang.h>
@@ -31,9 +60,6 @@ struct isc_bloomlimit {
 	isc_uint32_t bl_size;
 	isc_uint32_t bl_hashes;
 	isc_uint32_t bl_random;
-	isc_uint32_t bl_log;
-	isc_uint32_t bl_trunc;
-	isc_uint32_t bl_drop;
 	isc_uint32_t *bl_table;
 };
 
