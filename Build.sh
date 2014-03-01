625fe18b4a0f743a5f381e9a8316a2adabb6c34f
Tony Finch <dot@dotat.at>
2014-03-01 21:21:12 +0000

    build fixes

--- Build.sh
+++ Build.sh
@@ -30,7 +30,7 @@ export PYTHON=/opt/python-2.7.6+0/bin/python2.7
 LDFLAGS="-Wl,-R$PATH_OpenSSL/lib"
 export LDFLAGS
 
-./configure --enable-threads --with-readline \
+./configure --enable-threads --with-readline --with-pkcs11=no \
 	--with-openssl=$PATH_OpenSSL --without-gost --without-gssapi \
 	--prefix=$PREFIX --mandir=$PREFIX/man \
 	--localstatedir=/spool/bind/var \
@@ -44,8 +44,8 @@ ln -s bin $PREFIX/sbin
 cp ../nsdiff/nsdiff $PREFIX/bin
 cp doc/arm/*.html doc/arm/Bv9ARM.pdf $PREFIX/doc
 
-make -j3 all
-make -j3 install
+make all
+make install
 
 echo ==
 echo ==  $(date +%Y-%m-%d.%H:%M:%S%z) $PREFIX done
